f47d304972eb39202a98fb31340247a9826cf4f2
espinosarodrigyek-sketch <espinosarodrigyek@gmail.com>
2026-07-01 11:21:53 -0500

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -1,9 +1,14 @@
-"""
-Django settings for gym project.
-"""
 import os
 import sys
 from pathlib import Path
+import dj_database_url
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 
 try:
     from dotenv import load_dotenv
@@ -81,10 +86,9 @@ WSGI_APPLICATION = 'gym.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
